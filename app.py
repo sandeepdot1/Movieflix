4fde12eb1df1feb2ca31acd34a03535353497da1
sandeepdot1 <sandeeppanigrahi3570@gmail.com>
2021-07-09 17:08:38 +0530

    added the folder and folder structure of the project

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, session
+from flask_session import Session
+from werkzeug.utils import redirect
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def index():
+    if not session.get("name"):
+        return redirect("/login")
+    return render_template("index.html")
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    if request.method == "POST":
+        session["name"] = request.form.get('name')
+        return redirect("/")
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session["name"] = None
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
